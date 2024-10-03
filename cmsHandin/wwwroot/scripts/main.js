0c7c9a405d7bf38c9d29efc1cdf9398cca253bd8
Andreas Fredriksson <afredriksson89@gmail.com>
2024-10-03 10:50:14 +0200

    Search Box

--- cmsHandin/wwwroot/scripts/main.js
+++ cmsHandin/wwwroot/scripts/main.js
@@ -1,11 +1,13 @@
 import { fadeInOnScroll } from './effectScript.js';
 import { rotateOnScroll } from './effectScript.js';
 import { validateForm } from './validationScript.js';
-
+import { toggleSearchBox } from './searchScript.js';
+ 
 
 document.addEventListener('DOMContentLoaded', function () {
     fadeInOnScroll();
     rotateOnScroll();
     validateForm();
+    toggleSearchBox();
 });
 
